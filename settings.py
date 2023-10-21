845a195873f37222eb259a15d1efba41b4e95bde
w3bgr3p <113336090+w3bgr3p@users.noreply.github.com>
2023-10-21 02:51:17 -0500

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,15 @@
+# settings.py
+
+# Microsoft Graph API Credentials
+CLIENT_ID = 'ПОЛУЧИ НА САЙТЕ'
+CLIENT_SECRET = 'ПОЛУЧИ НА САЙТЕ'
+REFRESH_TOKEN = 'ПОЛУЧИ ИЗ auth2.py'
+REDIRECT_URI = 'http://localhost'  # Это может быть http://localhost
+AUTHORITY = 'https://login.microsoftonline.com/common'
+SCOPE = ["https://graph.microsoft.com/Mail.Read"]
+
+# Telegram Credentials
+TELEGRAM_BOT_TOKEN = 'TOKEN'
+TELEGRAM_CHANNEL_ID = '-100XXXXXXXXXX'
+LOGGING_LEVEL = 'DEBUG'
+LAST_EMAIL_ID = None
